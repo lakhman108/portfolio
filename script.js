518455110556f73dcc98eb7b2111567c5ab9e337
lakhman108 <119241790+lakhman108@users.noreply.github.com>
2023-04-17 10:43:31 +0530

    done

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 var typed = new Typed(".multiple-text", {
     strings: ["softwere developer", "Vedio Editor", "Web Designer","Programmer"],
-    typespeed:10,
-    backtypespeed:10,
-    backdealy:1000,
-    loop:true
-
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
 });
