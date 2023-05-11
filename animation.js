6243b17a2c1ea7154fe2d97cae919d27723e87e6
lakhman108 <119241790+lakhman108@users.noreply.github.com>
2023-05-11 10:55:03 +0530

    half done

---
+++ animation.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Proeffiency in C", "Doing Compititive Coding C++", "HTML and CSS","Php For Backend","JavaScript","SQL","Data Structures"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
