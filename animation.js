388610edfdc6bba5df6684210f630a64be3d6c43
lakhman108 <119241790+lakhman108@users.noreply.github.com>
2023-05-19 11:28:07 +0530

    ok done

--- animation.js
+++ animation.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text", {
-    strings: ["Proeffiency in C", "Doing Compititive Coding C++", "HTML and CSS","Php For Backend","JavaScript","SQL","Data Structures"],
+    strings: ["Proeffiency in C", "Doing Compititive Coding", "Frontend Development","Backend","JavaScript","SQL","Data Structures"],
     typeSpeed: 100,
     backSpeed: 50,
     backDelay: 1000,
